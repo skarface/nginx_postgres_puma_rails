29006a2c2623ef32d91b425a1a8270b223830332
Marco Martins <marko.double@gmail.com>
2018-10-04 15:31:14 +0200

    App from scrach

--- rails_app/config/routes.rb
+++
@@ -1,8 +0,0 @@
-Rails.application.routes.draw do
-  get 'home/index'
-
-  resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  root to: 'home#index'
-end
